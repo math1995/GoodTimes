960b064820530ed462ca01c46d3cfa96b0413cc5
Mathieu Levi <levi_m@epitech.eu>
2015-10-19 22:32:53 +0200

    lol

--- wp-config.php
+++ wp-config.php
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'sA,q4,EE?WAM]q4i+zxC+&DDpUc 2#K?k:fkho_*PxvRlIbAMx(-|>=8v5HEvx8N');
-define('SECURE_AUTH_KEY',  'y )|Kql7<AIB>6Hx_!z*DcBb9H+QcYg(_B+pA|f~,GY:g(p?DQRnrJ_/W mvrH>X');
-define('LOGGED_IN_KEY',    'L3k9w,<MbrfJg;1&D0wmdzGtKY+#Yd!04 _L??b&h,mw:Oj.+j(+DyF-bA9t!k9!');
-define('NONCE_KEY',        '%8AM[i{z)k< aQUd*82NkLW06E~,})?w`WvENd:tef$W6^X[W!,H.D{M50> c^;2');
-define('AUTH_SALT',        'yI!.C))ESkO~e#.M|H2xAk*wz51CR63Zet|*0pUbS|V*CA>8j!.9eaAU?~J!kq*m');
-define('SECURE_AUTH_SALT', '3`Do@h+-^K$Ri)8tsDgBvu[]7[x/~&/gxx*U]wc4xJZk[E9!`TSrmPF-ewa~x2lH');
-define('LOGGED_IN_SALT',   'iP]S}N>c)y+1RW*4yZXg`:kU.)g@y~@$/|+`O&-~CGE_g~__a:4|k+f-[4H_|)E}');
-define('NONCE_SALT',       'euJ>5RWrGWTS}i5.~0(uu+9P=)zH}OfLKLrCuJL>adZ&q)mru74-dr:N>r,9(2wJ');
+define('AUTH_KEY',         '');
+define('SECURE_AUTH_KEY',  '');
+define('LOGGED_IN_KEY',    '');
+define('NONCE_KEY',        '');
+define('AUTH_SALT',        '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT',   '');
+define('NONCE_SALT',       '');
 
 /**#@-*/
 
