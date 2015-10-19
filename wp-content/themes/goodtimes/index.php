415aa177a3ae73af59f2da169bd1ba7c3993005e
Rémi <bonnet.rem@gmail.com>
2015-10-19 22:27:11 +0200

    Theme

---
+++ wp-content/themes/goodtimes/index.php
@@ -0,0 +1,34 @@
+<html>
+ 
+<head>
+ 
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+ 	
+  <title></title>
+ 
+</head>
+ 
+<body>
+	<nav class="navbar" role="navigation">
+			<div id="ligne"></div>
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<div class="navbar-brand hidden-lg hidden-md">Menu</div>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<div class="nav navbar-nav">
+							<ul class="navbar-nav">
+								<li class="page_item"><a href="#accueil">Accueil</a></li>
+							</ul>
+						</div>
+					</div>				
+	</nav>  
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/theme.js"></script>
+ 
+</body>
+ 
+</html>
