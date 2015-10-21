30192babe27493c88081db7c5adc7702fb287cc7
Rémi <bonnet.rem@gmail.com>
2015-10-21 09:25:32 +0200

    re

--- page-home.php
+++ page-home.php
@@ -5,35 +5,55 @@
 <section class="slider"></section>
 
 <section class="moment">
-	<article class="matinee">
-		<p>Matinée</p>
-	</article>
-	<article class="journee">
-		<p>Journée</p>
-	</article>
+	<div class="row">
+		<article class="matinee">
+			<p>Matinée</p>
+		</article>
+		<article class="journee">
+			<p>Journée</p>
+		</article>
 
-	<article class="soiree">
-		<p>Soirée</p>
-	</article>
+		<article class="soiree">
+			<p>Soirée</p>
+		</article>
+	</div>
 </section>
 
-<section class="recetteduj">
-	<article>
-		<h2>La recette !</h2>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas orci ante, porta quis mollis a, efficitur
-			non risus. Nulla interdum leo at massa cursus, sed eleifend sapien mollis. Mauris commodo at nunc ac consequat. 
-			Integer rutrum arcu justo, eget luctus lacus mattis egestas. Sed suscipit venenatis ante, at molestie ligula accumsan non. 
-			Suspendisse lectus dolor, tincidunt eu tortor laoreet, faucibus porta ipsum. Proin aliquam ultricies interdum. Aenean 
-			fringilla sapien sit amet leo sagittis, vel vehicula ex placerat. Donec consectetur sapien id gravida rutrum.
-			at lorem tempor, tempus odio ut, interdum est. Nulla sit amet nisl mollis, fringilla sapien non, euismod ligula.
-		</p>
-		<a href="#">
-			<div class="lireplus">
-				<p>Lire la suite</p>
-			</div>
-		</a>
-	</article>
+<section>
+	<div class="row">
+		<article class="recetteduj">
+			<h2>La recette !</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas orci ante, porta quis mollis a, efficitur
+				non risus. Nulla interdum leo at massa cursus, sed eleifend sapien mollis. Mauris commodo at nunc ac consequat. 
+				Integer rutrum arcu justo, eget luctus lacus mattis egestas. Sed suscipit venenatis ante, at molestie ligula accumsan non. 
+				Suspendisse lectus dolor, tincidunt eu tortor laoreet, faucibus porta ipsum. Proin aliquam ultricies interdum. Aenean 
+				fringilla sapien sit amet leo sagittis, vel vehicula ex placerat. Donec consectetur sapien id gravida rutrum.
+				at lorem tempor, tempus odio ut, interdum est. Nulla sit amet nisl mollis, fringilla sapien non, euismod ligula.
+			</p>
+			<a href="#">
+				<div class="lireplus">
+					<p>Lire la suite</p>
+				</div>
+			</a>
+		</article>
+		<article class="plusconsulte">
+			<h2>Les plus consultés</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas orci ante, porta quis mollis a, efficitur
+				non risus. Nulla interdum leo at massa cursus, sed eleifend sapien mollis. Mauris commodo at nunc ac consequat. 
+				Integer rutrum arcu justo, eget luctus lacus mattis egestas. Sed suscipit venenatis ante, at molestie ligula accumsan non. 
+				Suspendisse lectus dolor, tincidunt eu tortor laoreet, faucibus porta ipsum. Proin aliquam ultricies interdum. Aenean 
+				fringilla sapien sit amet leo sagittis, vel vehicula ex placerat. Donec consectetur sapien id gravida rutrum.
+				at lorem tempor, tempus odio ut, interdum est. Nulla sit amet nisl mollis, fringilla sapien non, euismod ligula.
+			</p>
+			<a href="#">
+				<div class="lireplus">
+					<p>Lire la suite</p>
+				</div>
+			</a>
+		</article>
+	</div>
 </section>
 
 <?php get_footer(); ?>
