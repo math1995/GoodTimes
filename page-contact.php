f197cda87e7312e4f8ffc6acd0c2e1bac38152dc
Noems <noemie.ferret@hetic.net>
2015-10-21 11:50:01 +0200

    page-contact

--- page-contact.php
+++ page-contact.php
@@ -1,7 +1,19 @@
 <?php /* Template Name: page-contact.php */ ?>
 
 <?php get_header(); ?>
+ 	
 
-<p><?php the_field('nom_&_prenom'); ?></p>
-<p><?php the_field('email'); ?></p>
-<textarea><?php the_field('ecrire_un_message'); ?></textarea>
+ <section class="container">
+           <div class="bg-img"></div>
+            <form method = "POST">
+                <p><?php the_field('nom_&_prenom'); ?></p>
+                <p><?php the_field('email'); ?></p>
+                <p><?php the_field('ecrire_un_message'); ?></p>
+            </form>
+        
+</section>
+
+<?php get_footer(); ?>
+
+ 
+
