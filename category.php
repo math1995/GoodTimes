d72081064a999577abb1cf3930ba43fb9f413282
Benjamin Chareyron <BenChrn@MacBook-Pro-de-Benjamin-3.local>
2015-10-21 17:55:13 +0200

    end into category

--- category.php
+++ category.php
@@ -17,20 +17,40 @@
 
 <div class="container">
 
-<div class="clear"></div>
-<section class="eventsList">
-	<article class="eventItem col-xs-12 col-md-9">
-		<div class="eventThumbnail"></div>
-		<div class="eventContent">
-			<h3 class="eventTitle">Soirée cocktail</h3>
-			<p class="eventDescri">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus tempore distinctio beatae nisi est iste! Dolor odit, quidem ducimus necessitatibus in, id dolorum atque unde numquam repellat. Voluptatibus, corporis, obcaecati.</p>
+	<div class="clear"></div>
+	<section class="eventsList">
+		<article class="eventItem col-xs-12 col-md-9">
+			<div class="eventThumbnail col-xs-12 col-md-4"></div>
+			<div class="eventContent">
+				<h3 class="eventTitle">Soirée cocktail</h3>
+				<p class="eventDescri">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus tempore distinctio beatae nisi est iste! Dolor odit, quidem ducimus necessitatibus in, id dolorum atque unde numquam repellat. Voluptatibus, corporis, obcaecati.</p>
+			</div> 
 			<aside class="eventInfos">
-				<span class="eventDate"></span>
-				<span class="eventLength"></span>
-				<span class="readMore"></span>
+					<span class="eventDate">postée le : 12.12.12</span>
+					<span class="separator">/</span>
+					<span class="eventLength">durée : 3H45</span>
+					<a href="" class="readMore">Lire la suite</a>
+					<div class="clear"></div>
 			</aside>
-		</div> 
-	</article>
-</section>
+		</article>
+	</section>
 
+	<section class="sidebar col-xs-12 col-md-3">
+		<div class="searchbar">
+			<input type="text" class="searchbarContent" placeholder="&#xf002;" style="font-family:FontAwesome;">
+			</input>
+		</div>
+		<div class="categoriesContent">
+			<h3>Nos autres recettes</h3>
+			<ul class="categoriesList">
+				<li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+				<li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+				<li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+				<li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+				<li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+				<li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+			</ul>
+		</div>
+	</section>
+</div>
 <?php get_footer(); ?>
