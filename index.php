c1abc1fa3b2f0ece6a71595393a9571ca3efee97
Mathieu Levi <levi_m@epitech.eu>
2015-10-21 09:46:09 +0200

    correction

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php get_header(); ?>
+<?php get_template_part('page-home')
 <?php get_footer(); ?>
--- page-contact.php
+++ page-contact.php
@@ -1,3 +1,7 @@
 <?php /* Template Name: page-contact.php */ ?>
 
 <?php get_header(); ?>
+
+<p><?php the_field('nom_&_prenom'); ?></p>
+<p><?php the_field('email'); ?></p>
+<textarea><?php the_field('ecrire_un_message'); ?></textarea>
