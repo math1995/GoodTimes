bded99d6bc7630397e78e77b4900ec0898da584c
Benjamin Chareyron <BenChrn@MacBook-Pro-de-Benjamin-3.local>
2015-10-22 10:25:36 +0200

    init inté single.php

---
+++ 404.php
@@ -0,0 +1,67 @@
+<?php /* Template Name: page-categorie.php */ ?>
+
+
+<?php get_header(); ?>
+</div>
+</div>
+
+<div class="headerCategory">
+    <div class="headerPic"></div>
+    <div class="navbarCategory">
+        <div class="container">
+        <h2>Soirées > Soirée Cocktail</h2>
+        </div>
+    </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="singleLeft col-xs-12 col-md-9">
+        <section class="singleEventItem">
+            <div class="singleEventHeader">
+                <h2>Ceci est un titre</h2>
+            </div>
+            <div class="singleEventContent">
+                <div class="singleEventThumbnail col-xs-12 col-md-3">
+                    <img src="http://lorempixel.com/200/200" alt="">
+                </div>
+                <div class="singleEventInfos col-xs-12 col-md-9">
+                    <ul class="ingredients col-xs-6">
+                        <span>Ingrédients</span>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <ul class="metadata col-xs-6">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <p></p>
+            </div>   
+        <div class="clear"></div>
+        </section>
+    </div>
+
+   <section class="sidebar singleSidebar col-xs-12 col-md-3">
+        <div class="searchbar">
+            <input type="text" class="searchbarContent" placeholder="&#xf002;" style="font-family:FontAwesome;">
+            </input>
+        </div>
+        <div class="categoriesContent">
+            <h3>Nos autres recettes</h3>
+            <ul class="categoriesList">
+                <li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+                <li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+                <li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+                <li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+                <li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+                <li class="categoriesItem"><a href="">Lorem ipsum catégorie</a></li>
+            </ul>
+        </div>
+    </section>
